ce5eeefffbffc05495de1b898d603c9de4a0ec6e
Ronnie Ghose <ronnie.ghose@gmail.com>
2015-03-19 22:26:36 -0400

    try to push

--- disk/src/com/treode/disk/edit/PageDispatcher.scala
+++ disk/src/com/treode/disk/edit/PageDispatcher.scala
@@ -14,27 +14,55 @@
  * limitations under the License.
  */
 
-
- package com.treode.disk.edit
-
+package com.treode.disk.edit
+import com.treode.async.{Async, Callback, Scheduler}, Async.supply, Callback.ignore
 import com.treode.async.io.File
 import com.treode.async.Async
 import com.treode.buffer.PagedBuffer
-import com.treode.disk.Dispatcher
 import scala.collection.mutable.UnrolledBuffer
-import com.treode.async.{Async, Callback, Fiber, Scheduler }, Async.async
+import com.treode.async.implicits._
 
+private class PageWriter(dsp: PageDispatcher,val file: File)
+ {
+  val bits = 10
+  val buffer = PagedBuffer (bits)
+  var pos : Long = 0
+  val dispatcher = dsp
+  
+  listen() run (ignore)
 
-private class PageDispatcher(private var counter: Long)
- (implicit
-  scheduler: Scheduler
-) extends Dispatcher [(String, Callback[(Long, Int)])] 
-{
-	def write (data: String) : Async [(Long, Int)] = {
-		async { cb =>
-		val batch = (data, cb)
-		send(batch) 
-	}
+  def listen(): Async[Unit] = 
+    for { 
+      (_, strings) <- dispatcher.receive()    //returns (unrolledBuffer,cb)
+      _ <- write(strings)
+    } yield {
+      listen() run (ignore)
+    }
+    
+  /**
+   * Write `data` into the file asynchronously, using a write buffer. Returns
+   * the position of the writer and length written, if successful.
+   */
+  def write (data: UnrolledBuffer [(String, Callback[(Long, Int)])]) : Async [(Long, Int)] = {
+    var writePositions : Array[(Long,Int)] = new Array[(Long,Int)](data.length);
+    var i = 0
+    var beforeWritesPos = buffer.writePos
+    for (s <- data) {
+      var beforeEachWritePos = buffer.writePos
+      buffer.writeString (s._1)
+      writePositions(i) = (buffer.writePos, buffer.writePos - beforeEachWritePos)
+      i+=1
+    }
+    for {
+      _ <- file.flush (buffer, pos)
+    } yield {
+      buffer.clear ()
+      var q = 0
+      for (s <- data){
+        s._2.pass(writePositions(q))
+        q+=1
+      }
+      (buffer.writePos, buffer.writePos - beforeWritesPos)
+    }
+  }
 }
-}
-
